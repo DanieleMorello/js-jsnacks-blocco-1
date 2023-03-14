f07bb6e7c31512a5f8a1ec8ac980f7e57b022bad
DanieleMorello <danielemorello@live.it>
2023-03-14 11:40:59 +0100

    Initial commit Add 2 allert

---
+++ jsnack1/script.js
@@ -0,0 +1,14 @@
+/*
+  L’utente inserisce due numeri in successione, con due prompt.
+  Il software stampa il maggiore.
+*/
+
+// Tool
+// -prompt
+
+// L’utente inserisce due numeri in successione, con due prompt.
+const userNumber = prompt('inserisci un numero')
+console.log(userNumber);
+const otherNumber = prompt('inserisci un\' altro numero')
+console.log(otherNumber);
+// Il software stampa il maggiore.
